cfc986acdada76f86b3da8c6df45b57b9af5253b
Julia Pettere <juliapettere@D-10-157-59-213.dhcp4.washington.edu>
2016-02-22 23:00:22 -0800

    added required features

---
+++ Twitter/Tweet.swift
@@ -0,0 +1,13 @@
+//
+//  Tweet.swift
+//  Twitter
+//
+//  Created by Julia Pettere on 2/21/16.
+//  Copyright © 2016 codepath. All rights reserved.
+//
+
+import UIKit
+
+class Tweet: NSObject {
+
+}
---
+++ Twitter/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  Twitter
+//
+//  Created by Julia Pettere on 2/21/16.
+//  Copyright © 2016 codepath. All rights reserved.
+//
+
+import UIKit
+
+class User: NSObject {
+
+}
